d282f79098b3e8dbc1f1ec0ace8b72760fddd0c4
nevmx <maxsnev@gmail.com>
2017-02-24 12:33:02 -0500

    Added MainController class

---
+++ src/finalproject/MainController.java
@@ -0,0 +1,23 @@
+package finalproject;
+
+import lejos.hardware.*;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.*;
+import lejos.robotics.SampleProvider;
+
+public class MainController {
+	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+	
+	public static void main(String[] args) {
+		
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);	
+		
+	}
+
+}
