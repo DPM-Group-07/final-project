7b00bf7376dd71b3132492435bcce502059bd7cf
nevmx <maxsnev@gmail.com>
2017-03-19 13:54:41 -0400

    Changed speeds for arm movement

--- src/finalproject/MainController.java
+++ src/finalproject/MainController.java
@@ -151,8 +151,10 @@ public class MainController {
 		// TODO: Move this to Shooter class.
 		leftLaunchMotor.setAcceleration(1000);
 		rightLaunchMotor.setAcceleration(1000);
-		leftLaunchMotor.setSpeed(100);
-		rightLaunchMotor.setSpeed(100);
+		
+		leftLaunchMotor.setSpeed(25);
+		rightLaunchMotor.setSpeed(25);
+		
 		leftLaunchMotor.rotate(90,true);
 		rightLaunchMotor.rotate(90,false);
 		
